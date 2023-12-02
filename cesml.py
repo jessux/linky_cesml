7c23b21750b4c6328b7e3c525d6acaec900a49c2
Gabriel KAHLOUCHE <gabriel.kahlouche@gmail.com>
2023-12-02 10:42:20 +0100

    Update cesml.py

--- cesml.py
+++ cesml.py
@@ -6,12 +6,13 @@ import urllib3
 urllib3.disable_warnings()
 
 class Linky():
-    def __init__(self,username,password,client_id):
+    def __init__(self,username,password,client_id,proxy={}):
       self.session=requests.Session()
       self.username = username
       self.password = password
       self.client_id = client_id
       self.token = ""
+      self.proxy = proxy
       
     def get_accesstoken(self,resp, *args, **kwargs):
       at = re.search("\#(?:access_token)\=([\S\s]*?)\&",resp.url)
@@ -24,9 +25,9 @@ class Linky():
       headers = {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
-      response = self.session.post(url, headers=headers, data=payload,proxies={"https":"http://127.0.0.1:9000"},verify=False)
+      response = self.session.post(url, headers=headers, data=payload,proxies=self.proxy,verify=False)
       url = "https://moncompte-cesml-grd-eld.multield.net/application/auth/authorize-implicit-internet?redirect_uri=https%3A%2F%2Fmoncompte-cesml-grd-eld.multield.net%2Fautorisation-callback.html&response_type=token&client_id=aelGRD"
-      response = self.session.get( url, headers={}, data={},proxies={"https":"http://127.0.0.1:9000"},verify=False,hooks=dict(response=self.get_accesstoken))
+      response = self.session.get( url, headers={}, data={},proxies=self.proxy,verify=False,hooks=dict(response=self.get_accesstoken))
       return token
     
     def getMesures(self,dateDebut=datetime.datetime(2023,11,16,0,0,0,0)):
@@ -73,5 +74,5 @@ class Linky():
         'authorization': 'Bearer '+self.loginCESML()
         
       }
-      response = s.post(url, headers=headers, data=payload,proxies={"https":"http://127.0.0.1:9000"},verify=False)
+      response = s.post(url, headers=headers, data=payload,proxies=self.proxy,verify=False)
       return response.json()
